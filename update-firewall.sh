841c7b60fee9fd3b551eef34c9ab4e7bfd95daef
aaronstuder <aaronstuder@gmail.com>
2020-06-14 07:01:18 -0400

    Update update-firewall.sh

--- update-firewall.sh
+++ update-firewall.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-echo
-echo "Removing Existing Rules..."
-rm /etc/firewalld/zones/public.xml
-rm /etc/firewalld/zones/public.xml.old
-firewall-cmd --reload
+
 echo
 echo "Reading Domain List..."
 readarray domains < domain.list
@@ -11,7 +7,6 @@ for i in ${domains[@]}; do
         IP=`dig +short $i`
         echo "$i = $IP"
         echo "$IP" >> ip.tmp
-
 done
 
 readarray a < ip.tmp
@@ -19,14 +14,14 @@ echo
 echo "Updating Rules..."
 
 for i in ${a[@]}; do
-        firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address='"$i/32"' accept'
+        ufw allow from $i
 done
 rm ip.tmp
 echo
 echo "Reloading Firewall..."
-firewall-cmd --reload
+ufw reload
 echo
-firewall-cmd --list-all
+ufw status numbered
 echo
 echo "Success!"
 echo
