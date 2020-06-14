c0ec8ba8e459890fcd1086d2468d3e837247033d
aaronstuder <aaronstuder@gmail.com>
2020-06-14 08:59:35 -0400

    Update update-firewall.sh

--- update-firewall.sh
+++ update-firewall.sh
@@ -1,28 +1,45 @@
-#!/bin/bash
+
+ALLOWED_HOSTS_URL="https://sshfs.net/allowed_hosts.txt"
+ALLOWED_IPS_URL="https://sshfs.net/allowed_ips.txt"
 
 echo
-echo "Reading Domain List..."
-readarray domains < domain.list
+echo "Downloading Allow Files..."
+echo
+
+wget -nv $ALLOWED_HOSTS_URL -O allowed.hosts
+wget -nv $ALLOWED_IPS_URL -O allowed.ips
+
+echo
+echo "Resolving Allowed Hosts..."
+echo
+
+readarray domains < allowed.hosts
 for i in ${domains[@]}; do
         IP=`dig +short $i`
         echo "$i = $IP"
         echo "$IP" >> ip.tmp
 done
 
-readarray a < ip.tmp
 echo
-echo "Updating Rules..."
+echo "Adding Allowed IP Addresses..."
+echo
+cat allowed.ips
+cat allowed.ips >> ip.tmp
+echo
+echo
+echo "Updating Firewall Rules..."
+echo
 
+readarray a < ip.tmp
 for i in ${a[@]}; do
         ufw allow from $i
 done
 rm ip.tmp
+
 echo
-echo "Reloading Firewall..."
-ufw reload
 echo
 ufw status numbered
 echo
-echo "Success!"
+echo "Done!"
 echo
 exit
